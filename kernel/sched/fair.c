MotorolaMobilityLLC/kernel-slsi

[COMMON] sched: fair/ehmp: Add to estimate core/cluster power down.
after moving task, previous cpu/cluster can be powered down.
so it should be consider it when idle power was calculated.

Change-Id: I13d954a1640940a7be93d0429248be8802fd615c
Signed-off-by: lakkyung.jung <lakkyung.jung@samsung.com>

kernel/sched/fair.c
@@ -5710,6 +5710,18 @@ static int group_idle_state(struct energy_env *eenv, int cpu_idx)
 	 */
 	max_idle_state_idx = sg->sge->nr_idle_states - 2;
 	new_state = grp_util * max_idle_state_idx;
+
+	/*
+	 * after moving, previous cpu/cluster can be powered down,
+	 * so it should be consider it when idle power was calculated.
+	 */
+	if (sched_feat(EXYNOS_HMP)) {
+		new_state = exynos_estimate_idle_state(cpu_idx, sched_group_span(sg),
+						max_idle_state_idx, sg->group_weight);
+		if (new_state)
+			return new_state;
+	}
+
 	if (grp_util <= 0) {
 		/* group will have no util, use lowest state */
 		new_state = max_idle_state_idx + 1;

xthorfinnx/exynos-linux-stable

kernel/sched/fair.c
@@ -6175,6 +6175,18 @@ static int group_idle_state(struct energy_env *eenv, int cpu_idx)
 		 */
 		int max_idle_state_idx = sg->sge->nr_idle_states - 2;
 		int new_state = grp_util * max_idle_state_idx;
+
+		/*
+		* after moving, previous cpu/cluster can be powered down,
+		* so it should be consider it when idle power was calculated.
+		*/
+		if (sched_feat(EXYNOS_HMP)) {
+			new_state = exynos_estimate_idle_state(cpu_idx, sched_group_cpus(sg),
+							max_idle_state_idx, sg->group_weight);
+			if (new_state)
+				return new_state;
+		}
+
 		if (grp_util <= 0)
 			/* group will have no util, use lowest state */
 			new_state = max_idle_state_idx + 1;